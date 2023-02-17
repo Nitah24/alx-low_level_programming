c4092a7fb2c4800021baf9de8d2cbaff6dc2059d
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-16 18:35:32 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+/*Programm to print a line using puts()*/
 
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle\n");
+int main(void){
+ puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
