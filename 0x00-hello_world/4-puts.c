9d22bd22fc6185ea625ef82835c86db6672bc764
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-16 19:11:25 -0800

    Puts again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-/* program to print using puts */
 
-int main(void)
+int main(void) /* program to print using puts */
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle ");
 	return (0);
 }
