f768fe7c0479fdd3f3bc15c548fc85bd338b3d71
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-20 18:40:45 -0800

    Printing numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
