939bbcc48872591857217f224a6a7bef45b7cc78
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-20 19:52:04 -0800

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-/*Program to print the seze of various types*/
+/**
+ * main - Entry level
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	
-    char type_char;
-    int type_int;
-    long int type_longint;
-    long long int type_lnglongint;
-    float type_float;
 
-    printf("Size of a char: %zu byte\n", sizeof(type_char));
-        printf("Size of an int: %zu byte\n", sizeof(type_int));
-	    printf("Size of a long int: %zu byte\n", sizeof(type_longint));
-	    printf("Size of a long long int: %zu byte\n", sizeof(type_lnglongint));
-		  printf("Size of a float: %zu byte\n", sizeof(type_float));
+char type_char;
+int type_int;
+long int type_longint;
+long long int type_lnglongint;
+float type_float;
 
-    return (0);
+printf("Size of a char: %zu byte(s)\n", sizeof(type_char));
+printf("Size of an int: %zu byte(s)\n", sizeof(type_int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(type_longint));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(type_lnglongint));
+printf("Size of a float: %zu byte(s)\n", sizeof(type_float));
+
+return (0);
 }
