d6d29d36ada8540a5b3e489b252c5db4f5061d35
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-21 20:35:49 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alpabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -print the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+
+return ();
+}
