375adf4cc094b1afaa6c42340d416c63116c5547
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-24 15:32:56 -0800

    fixing betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,10 @@ void print_most_numbers(void)
 for (x = '0'; x <= '9'; x++)
 	{
 if (x == '2' || x == '4')
-					{
-					continue;
-					}
-			_putchar(x);
+{
+continue;
+}
+_putchar(x);
 	}
 	_putchar('\n');
 }
