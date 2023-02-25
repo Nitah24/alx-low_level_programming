c60d54fa9fd46b925a38c8773ee9e93060ce3dff
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-24 19:53:35 -0800

    more nubers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers- prints more numbers
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int x;
+	int i = 0;
+
+	while (i < 14)
+	{
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}
+	for ()
+	_putchar('\n');
+	i++;
+	}
+}
