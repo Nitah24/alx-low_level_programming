0b8f03d7720e247d60a2f672aca5e94aaf50cd99
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-02-28 13:28:58 -0800

    98 Battery street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98- takes a pointer to an int as parameter, updates the value to 98
+ * @n: parameter to be pointed to.
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+
+return;
+}
+
