a86c66fb428fdb672ddce5afd07613cb8cfcae51
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-03-06 17:59:42 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr-locates a substring
+ * @haystack: string to search
+ * @needle: string to find
+ * Return: ponter to needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (haystack + 7);
+			}
+		}
+	}
+	return (NULL);
+}
