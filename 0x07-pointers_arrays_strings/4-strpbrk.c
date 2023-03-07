5f1e983c7546fe2c643399e2d381a5782954f2e2
Nitah24 <122923585+Nitah24@users.noreply.github.com>
2023-03-06 17:31:50 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk -searches a string for any set of bytes
+ * @s: first string
+ * @accept: 2nd string
+ * Return: pointer to occurances
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
