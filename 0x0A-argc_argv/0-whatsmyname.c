77a142860e7de213874bb597de3bff4664332d2a
Nitah24 <anitandou2022@gmail.com>
2023-06-11 10:49:26 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * main - entry point
  * @argc: count of the arguments supplies to the program
  * @argv: an array of pointers to arguments
- * return: always 0 success
+ * Return: always 0 success
  */
 
 int main(int argc, char *argv[])
