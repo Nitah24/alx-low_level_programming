35723b6a3a63edf8227453fba3487beb0454980a
Nitah24 <anitandou2022@gmail.com>
2023-06-12 05:15:24 -0700

    print count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: count of arguments
+ * @argv: array of ponters to arguments
+ * Return: always 0 success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
