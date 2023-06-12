ab00b0d3d79d8e58eca69b55a2b15d1b9d9cd647
Nitah24 <anitandou2022@gmail.com>
2023-06-12 08:23:56 -0700

    multiplying arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: count of arguments
+ * @argv: array of strings
+ * Return: 0 success or 1 error
+ */
+
+int main(int argc, char *argv[])
+{
+	int times;
+
+	if (argc == 3)
+	{
+		while (argc--)
+		{
+		times = atoi(argv[1]) *  atoi(argv[2]);
+		}
+		printf("%d\n", times);
+	}
+	else
+	{
+	printf("Error\n");
+
+	}
+	return (1);
+}
