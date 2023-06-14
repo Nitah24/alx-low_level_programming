1d4ed34293f2f4f3f2fdf7403687ef79015d20a5
Nitah24 <anitandou2022@gmail.com>
2023-06-14 07:29:54 -0700

    libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,9 +2,11 @@
 
 /**
  * _putchar - writes a character  to stdio
+ * @c: character to be written
  * Return: On success 1
  * on error, -1
  */
+
 int _putchar(char c)
 {
 return (write(1, &c, 1));
