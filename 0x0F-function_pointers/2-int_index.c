3fb3c9b0b3de563a40186597bd0d0cb61c7962e4
Nitah24 <anitandou2022@gmail.com>
2023-06-27 00:46:35 -0700

    searching integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,22 +14,22 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array != NULL && cmp != NULL)
-	{
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		if (array != NULL && cmp != NULL)
 		{
-			if (cmp(array[i]))
+			for (i = 0; i < size; i++)
 			{
-				return (i);
+				if (cmp(array[i]))
+				{
+					return (i);
+				}
 			}
 		}
 	}
 	else
 	{
 		return (-1);
-	}
 	}
 	return (0);
 }
