f182ac2ae2cb98bd57405aa4723c59a738cd692d
Nitah24 <anitandou2022@gmail.com>
2023-06-30 11:21:43 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - strycture with dog details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: data structure containing data about dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
