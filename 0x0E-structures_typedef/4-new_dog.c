4ce8545b67c81aa6404481ab5a04fab7f83ee1bf
Nitah24 <anitandou2022@gmail.com>
2023-07-01 15:15:17 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new dog - function to create a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog_t type
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *max;
+	int nombre = 0, papa = 0;
+
+	if (name != NULL && owner != NULL)
+	{
+		nombre = strlen(name) + 1;
+		papa = strlen(owner) + 1;
+		max = malloc(sizeof(dog_t));
+
+		if (max == NULL)
+			return (NULL);
+		max->name = malloc(sizeof(char) * nombre);
+
+		if (max->name == NULL)
+		{
+			free(max);
+			return (NULL);
+		}
+		max->owner = malloc(sizeof(char) * papa);
+
+		if (max->owner == NULL)
+		{
+			free(max->name);
+			free(max);
+			return (NULL);
+		}
+		max->name = strcpy(max->name, name);
+		max->owner = strcpy(max->owner, owner);
+		max->age = age;
+	}
+	return (max);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * d: pointer to structure
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
