825234a128252dbab0c3f5e8b291a0e39c692ff3
Nitah24 <anitandou2022@gmail.com>
2023-07-04 19:04:07 -0700

    task 8

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int n;
+	unsigned int n = 0;
 	listint_t *tp = head;
 
 	while (tp && n < index)
@@ -18,8 +18,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		tp = tp->next;
 		n++;
 	}
-
-	if (!tp)
-		return (NULL);
-	return (tp);
+	return (tp ? tp : NULL);
 }
